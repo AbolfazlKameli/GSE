04c1a1930777ab5d63c0de999b810a93bc20b68e
abolfazl <abolfazlkameli0@gmail.com>
2024-10-27 22:16:49 +0330

    feat(init): initial project.

---
+++ core/configs/drf_spectacular.py
@@ -0,0 +1,7 @@
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'GSE API',
+    'DESCRIPTION': 'Documentation of API endpoints of GSE',
+    'VERSION': '0.1.0',
+    'is_superuser': True,
+    'is_authenticated': True
+}
