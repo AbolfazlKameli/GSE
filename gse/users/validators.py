7917fdec5e7555b2300c7b3a48163da9012c6634
abolfazl <abolfazlkameli0@gmail.com>
2024-10-30 18:31:18 +0330

    fix(models): add postal code validator.

--- gse/users/validators.py
+++ gse/users/validators.py
@@ -4,14 +4,14 @@ from django.core.exceptions import ValidationError
 
 
 def validate_iranian_phone_number(value):
-    pattern = r'^(\\+98|0)?9\\d{9}$'
+    pattern = '^(\\+98|0)?9\\d{9}$'
 
     if not re.match(pattern, value):
         raise ValidationError('یک شماره معتبر ایرانی وارد کنید.')
 
 
 def validate_postal_code(value):
-    pattern = r'^\d{10}$'
+    pattern = '^\\d{10}$'
 
     if not re.match(pattern, value):
         raise ValidationError('یک کد پستی معتبر وارد کنید.')
