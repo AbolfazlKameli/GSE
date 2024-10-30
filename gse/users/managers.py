4a087196ba06aa1926567d567533ce100e336981
abolfazl <abolfazlkameli0@gmail.com>
2024-10-30 17:28:35 +0330

    fix(manager): add address create to manager and make transactions atomic.

--- gse/users/managers.py
+++ gse/users/managers.py
@@ -1,10 +1,12 @@
 from django.apps import apps
 from django.contrib.auth.models import BaseUserManager
+from django.db import transaction
 
 from .choices import USER_ROLE_ADMIN, USER_ROLE_CUSTOMER
 
 
 class UserManager(BaseUserManager):
+    @transaction.atomic
     def create_user(self, email, password, role=USER_ROLE_CUSTOMER):
         if not email:
             raise ValueError('کاربران باید ایمیل داشته باشند.')
@@ -17,8 +19,12 @@ class UserManager(BaseUserManager):
         profile = apps.get_model('users', 'UserProfile')
         profile.objects.create(owner=user)
 
+        address = apps.get_model('users', 'Address')
+        address.objects.create(owner_profile=user)
+
         return user
 
+    @transaction.atomic
     def create_superuser(self, email, password):
         user = self.create_user(
             email=email,
