657ba59c80b894efe8f0ab2448b50f2101101a81
abolfazl <abolfazlkameli0@gmail.com>
2024-11-02 16:16:31 +0330

    fix(otp_code): use email instead of phone number in generate code.

--- gse/users/services.py
+++ gse/users/services.py
@@ -16,16 +16,16 @@ def register(*, email: str, password: str) -> User:
     return user
 
 
-def generate_otp_code(*, phone_number: str):
+def generate_otp_code(*, email: str):
     while True:
         otp_code: str = str(randint(10000, 99999))
 
         if not cache.get(f'otp_code_{otp_code}'):
-            cache.set(f'otp_code_{phone_number}', otp_code, timeout=300)
-            cache.set(f'otp_code_{otp_code}', phone_number, timeout=300)
+            cache.set(f'otp_code_{email}', otp_code, timeout=300)
+            cache.set(f'otp_code_{otp_code}', email, timeout=300)
             return otp_code
 
 
-def check_otp_code(*, phone_number: str, otp_code: str):
-    stored_code: str = cache.get(f'otp_code_{phone_number}')
+def check_otp_code(*, email: str, otp_code: str):
+    stored_code: str = cache.get(f'otp_code_{email}')
     return stored_code == otp_code
