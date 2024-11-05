3c46351eeb4c232f57d0062495c2248537b52315
abolfazl <abolfazlkameli0@gmail.com>
2024-11-05 18:17:26 +0330

    fix(dev_database): change development database to sqlite.

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -104,12 +104,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('PG_NAME'),
-        'USER': config('PG_USERNAME'),
-        'PASSWORD': config('PG_PASSWORD'),
-        'HOST': config('PG_HOST'),
-        'PORT': config('PG_PORT', cast=int),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 # caches
