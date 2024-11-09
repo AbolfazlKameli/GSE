e4a053b0f2034bad45f34fe8c30930a5476a2e99
abolfazl <abolfazlkameli0@gmail.com>
2024-11-09 22:27:14 +0330

    fix(category): fix category model.

--- gse/products/models.py
+++ gse/products/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class ProductCategory(models.Model):
     title = models.CharField(max_length=200)
-    slug = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     is_sub = models.BooleanField(default=False)
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='s_category', blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
