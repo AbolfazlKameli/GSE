c6ae3fd779f6a8f2ef6c3c9e941fa48b556cbcd5
abolfazl <abolfazlkameli0@gmail.com>
2024-11-09 22:32:21 +0330

    feat(products): add products model.

--- gse/products/models.py
+++ gse/products/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
 
@@ -8,3 +9,16 @@ class ProductCategory(models.Model):
     sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='s_category', blank=True, null=True)
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
+
+
+class Product(models.Model):
+    category = models.ManyToManyField(ProductCategory, related_name='products', blank=True)
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=250)
+    quantity = models.PositiveSmallIntegerField(validators=[MaxValueValidator(1000)])
+    description = models.TextField()
+    available = models.BooleanField(default=True)
+    unit_price = models.DecimalField(max_digits=15, decimal_places=0)
+    discount_percent = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)])
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
