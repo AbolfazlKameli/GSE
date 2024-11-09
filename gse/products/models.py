0f21e7c97e2c9c1d4a5cceabd49ab10749f9693c
abolfazl <abolfazlkameli0@gmail.com>
2024-11-09 22:03:55 +0330

    feat(category): add Product Category model.

--- gse/products/models.py
+++ gse/products/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.CharField(max_length=250)
+    is_sub = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
