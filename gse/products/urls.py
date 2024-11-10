cdc3d1989543204a22b2bb5807645a0309a3f7fe
abolfazl <abolfazlkameli0@gmail.com>
2024-11-10 22:43:55 +0330

    feat(products_list): add products list API.

--- gse/products/urls.py
+++ gse/products/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-
+    path('', views.ProductsListAPI.as_view(), name='products_list')
 ]
--- gse/products/views.py
+++ gse/products/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from rest_framework.generics import ListAPIView
+
+from .models import Product
+from .serializers import ProductListSerializer
+
+
+class ProductsListAPI(ListAPIView):
+    """
+    List all Product objects.
+    """
+    queryset = Product.objects.prefetch_related('details', 'media').all()
+    serializer_class = ProductListSerializer
