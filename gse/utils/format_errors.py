3f4ac36499ff4b2915f7324ff4a2ac8c2a45ab1d
abolfazl <abolfazlkameli0@gmail.com>
2024-11-11 16:52:33 +0330

    feat(format_errors): upgraded format errors to work with new error structure.

--- gse/utils/format_errors.py
+++ gse/utils/format_errors.py
@@ -1,2 +1,11 @@
 def format_errors(errors):
-    return {field: error[0] for field, error in errors.items()}
+    formatted_errors = {}
+    for field, error in errors.items():
+        if isinstance(error, list):
+            formatted_errors[field] = str(error[0])
+        elif isinstance(error, dict):
+            formatted_errors[field] = {key: str(val[0]) if isinstance(val, list) else str(val) for key, val in
+                                       error.items()}
+        else:
+            formatted_errors[field] = str(error)
+    return formatted_errors
