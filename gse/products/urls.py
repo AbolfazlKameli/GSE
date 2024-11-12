49815cac8c7d4bbd0bbb93676d72ffe856e9a932
abolfazl <abolfazlkameli0@gmail.com>
2024-11-12 10:03:41 +0330

    feat(create API): add create api and uri.

--- gse/products/urls.py
+++ gse/products/urls.py
@@ -5,6 +5,7 @@ from . import views
 app_name = 'products'
 
 urlpatterns = [
-    path('', views.ProductsListAPI.as_view(), name='products_list'),
-    path('<int:pk>/', views.ProductRetrieveAPI.as_view(), name='products_retrieve')
+    path('list/', views.ProductsListAPI.as_view(), name='products_list'),
+    path('<int:pk>/', views.ProductRetrieveAPI.as_view(), name='products_retrieve'),
+    path('', views.ProductCreateAPI.as_view(), name='product_create')
 ]
--- gse/products/views.py
+++ gse/products/views.py
@@ -1,10 +1,16 @@
+from rest_framework import status
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .models import Product
+from gse.utils.format_errors import format_errors
+from .selectors import (
+    get_all_products
+)
 from .serializers import (
     ProductDetailsSerializer,
-    ProductListSerializer
+    ProductListSerializer,
+    ProductCreateSerializer
 )
 
 
@@ -12,15 +18,15 @@ class ProductsListAPI(ListAPIView):
     """
     List all Product objects.
     """
-    queryset = Product.objects.prefetch_related('details', 'media').all()
+    queryset = get_all_products()
     serializer_class = ProductListSerializer
 
 
 class ProductRetrieveAPI(RetrieveAPIView):
     """
-    Retrieve a product object
+    Retrieve a product object.
     """
-    queryset = Product.objects.prefetch_related('details', 'media').all()
+    queryset = get_all_products()
     serializer_class = ProductDetailsSerializer
 
     def get(self, request, *args, **kwargs):
@@ -28,4 +34,25 @@ class ProductRetrieveAPI(RetrieveAPIView):
         return Response(
             data={'data': response.data},
             status=response.status_code
+        )
+
+
+class ProductCreateAPI(APIView):
+    """
+    Creates a Product object.
+    """
+    serializer_class = ProductCreateSerializer
+    queryset = get_all_products()
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data={'data': {'message': 'product Created.'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': format_errors(serializer.errors)}},
+            status=status.HTTP_400_BAD_REQUEST
         )
