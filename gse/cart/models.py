5c9c34cf797de09e9f83fe322054e3b9e0bef748
abolfazl <abolfazlkameli0@gmail.com>
2024-11-16 18:46:25 +0330

    feat(CartItem): add CartItem model.

--- gse/cart/models.py
+++ gse/cart/models.py
@@ -1,9 +1,19 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
+from gse.products.models import Product
 from gse.users.models import User
 
 
 class Cart(models.Model):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    quantity = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)], default=0)
     created_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
