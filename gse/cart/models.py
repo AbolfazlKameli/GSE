7421f92282de66a05c2475f61866daff893e3433
abolfazl <abolfazlkameli0@gmail.com>
2024-11-16 18:28:54 +0330

    feat(CartModle): add cart model.

--- gse/cart/models.py
+++ gse/cart/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+from gse.users.models import User
+
+
+class Cart(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
