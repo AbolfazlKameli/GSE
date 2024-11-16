6086498baeb6effc20621d8a80f6877345337ed8
abolfazl <abolfazlkameli0@gmail.com>
2024-11-16 23:03:03 +0330

    feat(CartItemSerializer): add CartItemSerializer.

--- gse/cart/serializers.py
+++ gse/cart/serializers.py
@@ -1,10 +1,17 @@
 from rest_framework import serializers
 
-from .models import Cart
+from .models import Cart, CartItem
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CartItem
+        exclude = ('cart',)
 
 
 class CartSerializer(serializers.ModelSerializer):
     owner = serializers.SlugRelatedField(read_only=True, slug_field='email')
+    items = CartItemSerializer(many=True)
 
     class Meta:
         model = Cart
