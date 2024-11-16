f2e7ae6fb283580760de20031e913f4d545996b9
abolfazl <abolfazlkameli0@gmail.com>
2024-11-16 18:31:19 +0330

    fix(CartModel): change relationship type from many to one to one to one.

--- gse/cart/models.py
+++ gse/cart/models.py
@@ -4,6 +4,6 @@ from gse.users.models import User
 
 
 class Cart(models.Model):
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cart')
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
     created_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
