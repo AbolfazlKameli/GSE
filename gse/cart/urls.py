aaed1c778c5d229fef7b6ca8da579d84b9151502
abolfazl <abolfazlkameli0@gmail.com>
2024-11-16 22:55:07 +0330

    feat(CartRetrieve): add CartRetrieve API.

--- gse/cart/urls.py
+++ gse/cart/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'cart'
 
 urlpatterns = [
-
+    path('<int:pk>/', views.CartRetrieveAPI.as_view(), name='cart_retrieve')
 ]
--- gse/cart/views.py
+++ gse/cart/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import RetrieveAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .selectors import get_all_carts
+from .serializers import (
+    CartSerializer
+)
+
+
+class CartRetrieveAPI(RetrieveAPIView):
+    serializer_class = CartSerializer
+    queryset = get_all_carts()
+
+    def get(self, request, *args, **kwargs):
+        response = super().get(request, *args, **kwargs)
+        return Response(
+            data={'data': response.data},
+            status=status.HTTP_200_OK
+        )
