0fe4642bf445cdb3a3332e1544fb0123994c81c7
abolfazl <abolfazlkameli0@gmail.com>
2024-11-17 22:42:04 +0330

    fix(format_error): update and fix format_errors.

--- gse/utils/format_errors.py
+++ gse/utils/format_errors.py
@@ -2,10 +2,12 @@ def format_errors(errors):
     formatted_errors = {}
     for field, error in errors.items():
         if isinstance(error, list):
-            formatted_errors[field] = error[0]
+            if all(isinstance(item, dict) for item in error):
+                formatted_errors[field] = [format_errors(item) for item in error]
+            else:
+                formatted_errors[field] = error[0]
         elif isinstance(error, dict):
-            formatted_errors[field] = {key: val[0] if isinstance(val, list) else val for key, val in
-                                       error.items()}
+            formatted_errors[field] = {key: val[0] if isinstance(val, list) else val for key, val in error.items()}
         else:
             formatted_errors[field] = error
     return formatted_errors
