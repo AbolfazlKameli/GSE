5c7c314bcf1e50dac66ea060e590aa08438a723d
abolfazl <abolfazlkameli0@gmail.com>
2024-11-18 19:36:43 +0330

    feat(inlines): add cart item inline.

--- gse/cart/admin.py
+++ gse/cart/admin.py
@@ -3,9 +3,15 @@ from django.contrib import admin
 from .models import Cart, CartItem
 
 
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    fields = ('product', 'quantity')
+
+
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner__email')
+    inlines = [CartItemInline]
 
 
 @admin.register(CartItem)
