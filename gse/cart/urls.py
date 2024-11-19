66ca2d15ff7198524b7d338718d44af18bb4101c
abolfazl <abolfazlkameli0@gmail.com>
2024-11-19 21:29:24 +0330

    feat(API): add CartItemUpdate API.

--- gse/cart/urls.py
+++ gse/cart/urls.py
@@ -7,4 +7,5 @@ app_name = 'cart'
 urlpatterns = [
     path('<int:pk>/', views.CartRetrieveAPI.as_view(), name='cart_retrieve'),
     path('add/', views.CartItemAddAPI.as_view(), name='cart_create'),
+    path('<int:pk>/update/', views.CartItemUpdateAPI.as_view(), name='cart_update')
 ]
--- gse/cart/views.py
+++ gse/cart/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -5,10 +6,11 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from gse.utils.format_errors import format_errors
+from .models import CartItem
 from .selectors import get_all_carts
 from .serializers import (
     CartSerializer,
-    CartItemAddSerializer
+    CartItemAddSerializer, CartItemUpdateSerializer
 )
 
 
@@ -35,6 +37,29 @@ class CartItemAddAPI(APIView):
             return Response(
                 data={'data': {'message': 'محصول با موفقیت به سبد خرید اضافه شد.'}},
                 status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': format_errors(serializer.errors)}},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+class CartItemUpdateAPI(APIView):
+    serializer_class = CartItemUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request, *args, **kwargs):
+        item: CartItem = get_object_or_404(CartItem, id=kwargs.get('pk'))
+        serializer = self.serializer_class(
+            data=request.data,
+            instance=item,
+            context={'product': item.product, 'request': request}
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data={'data': {'message': 'آیتم سبد خرید با موفقیت به روز شد.'}},
+                status=status.HTTP_200_OK
             )
         return Response(
             data={'data': {'errors': format_errors(serializer.errors)}},
