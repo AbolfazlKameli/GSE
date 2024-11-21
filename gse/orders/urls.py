89b2bd885f77032caf42079a3f8e02332d0d952a
abolfazl <abolfazlkameli0@gmail.com>
2024-11-22 01:01:25 +0330

    feat(API): add order create API.

--- gse/orders/urls.py
+++ gse/orders/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'orders'
 urlpatterns = [
-    path('<int:pk>/', views.OrderRetrieveAPI.as_view(), name='order_retrieve')
+    path('<int:pk>/', views.OrderRetrieveAPI.as_view(), name='order_retrieve'),
+    path('add/', views.OrderCreateAPI.as_view(), name='order_create')
 ]
--- gse/orders/views.py
+++ gse/orders/views.py
@@ -1,9 +1,11 @@
 from rest_framework import status
 from rest_framework.generics import RetrieveAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 
-from .serializers import OrderSerializer
 from .selectors import get_all_orders
+from .serializers import OrderSerializer, OrderCreateSerializer
 
 
 class OrderRetrieveAPI(RetrieveAPIView):
@@ -15,4 +17,22 @@ class OrderRetrieveAPI(RetrieveAPIView):
         return Response(
             data={'data': response.data},
             status=status.HTTP_200_OK
+        )
+
+
+class OrderCreateAPI(APIView):
+    serializer_class = OrderCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                data={'data': {'message': 'سفارش با موفقیت ایجاد شد.'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': serializer.errors}},
+            status=status.HTTP_400_BAD_REQUEST
         )
