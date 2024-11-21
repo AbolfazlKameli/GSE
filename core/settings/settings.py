30613faf7e2fd341f557bb4ca6f3f30b19bb77d4
abolfazl <abolfazlkameli0@gmail.com>
2024-11-21 15:01:17 +0330

    feat(orders): add orders app.

--- core/settings/settings.py
+++ core/settings/settings.py
@@ -58,6 +58,7 @@ LOCAL_APPS = [
     "gse.users.apps.UsersConfig",
     "gse.products.apps.ProductsConfig",
     "gse.cart.apps.CartConfig",
+    "gse.orders.apps.OrdersConfig"
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
