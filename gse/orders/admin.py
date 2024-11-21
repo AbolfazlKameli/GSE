00af4f135ad31228d3d44f573018291e6c2544d9
abolfazl <abolfazlkameli0@gmail.com>
2024-11-21 15:34:02 +0330

    feat(admin): add order item admin page.

--- gse/orders/admin.py
+++ gse/orders/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Order
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    readonly_fields = ('total_price',)
 
 
 @admin.register(Order)
@@ -8,3 +13,10 @@ class OrderAdmin(admin.ModelAdmin):
     list_display = ('id', 'owner__email', 'status')
     list_filter = ('status',)
     search_fields = ('owner__email',)
+    inlines = [OrderItemInline]
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order__id', 'product__id', 'total_price', 'quantity')
+    readonly_fields = ('total_price',)
