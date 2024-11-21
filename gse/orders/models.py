374855ba904fd081dd1194a97ea133c774fb0899
abolfazl <abolfazlkameli0@gmail.com>
2024-11-21 15:16:36 +0330

    feat(orders): add orders model.

--- gse/orders/models.py
+++ gse/orders/models.py
@@ -1,3 +1,18 @@
+from django.core.validators import MaxValueValidator
 from django.db import models
 
-# Create your models here.
+from gse.users.models import User
+from .choices import ORDER_STATUS_CHOICES, ORDER_STATUS_PENDING
+
+
+class Order(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    status = models.CharField(
+        choices=ORDER_STATUS_CHOICES,
+        verbose_name='وضعیت سفارش',
+        default=ORDER_STATUS_PENDING,
+        max_length=10
+    )
+    discount_percent = models.PositiveSmallIntegerField(validators=[MaxValueValidator(100)], default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
