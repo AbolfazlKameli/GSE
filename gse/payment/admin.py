169c0f066a46bdfddde3e408cc7c706955f829b8
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-11-30 22:48:22 +0330

    feat(payment): add payment model.

--- gse/payment/admin.py
+++ gse/payment/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Payment
+
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ('ref_id', 'status', 'id', 'response_code')
+    list_filter = ('status',)
--- gse/payment/models.py
+++ gse/payment/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from gse.orders.models import Order
+from .choices import PAYMENT_STATUS_CHOICES, PAYMENT_STATUS_PENDING
+
+
+class Payment(models.Model):
+    order = models.OneToOneField(Order, on_delete=models.CASCADE, related_name='payment')
+    authority_id = models.CharField(max_length=250)
+    ref_id = models.CharField(max_length=250)
+    amount = models.DecimalField(max_digits=15, decimal_places=0)
+    response_json = models.JSONField()
+    response_code = models.IntegerField()
+    status = models.CharField(
+        choices=PAYMENT_STATUS_CHOICES,
+        verbose_name='وضعیت پرداخت',
+        default=PAYMENT_STATUS_PENDING,
+        max_length=10
+    )
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.order.id} - {self.ref_id} - {self.status}'
