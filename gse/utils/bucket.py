429ab3b3313ab75db82c6b3346872539be4df246
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-11-30 21:47:28 +0330

    feat(bucket): add upload to the bucket file.

--- gse/utils/bucket.py
+++ gse/utils/bucket.py
@@ -1,3 +1,5 @@
+import io
+
 import boto3
 from django.conf import settings
 
@@ -20,7 +22,20 @@ class Bucket(metaclass=SingletonBucket):
             aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
             endpoint_url=settings.AWS_S3_ENDPOINT_URL,
         )
+        self.bucket_name = settings.AWS_STORAGE_BUCKET_NAME
 
     def delete_object(self, key):
-        self.connection.delete_object(Bucket=settings.AWS_STORAGE_BUCKET_NAME, Key=key)
+        self.connection.delete_object(Bucket=self.bucket_name, Key=key)
         return True
+
+    def upload_file(self, file_path: str, file_content: bytes, content_type: str = None) -> str:
+        try:
+            self.connection.upload_fileobj(
+                io.BytesIO(file_content),
+                self.bucket_name,
+                file_path,
+                ExtraArgs={"ContentType": content_type}
+            )
+            return file_path
+        except Exception as e:
+            raise Exception(f"Failed to upload file to S3: {e}") from e
