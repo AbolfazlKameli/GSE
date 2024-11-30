a7739e5d2315683d56af3dddd4b7872a0045a556
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-11-30 17:23:09 +0330

    fix

--- gse/cart/views.py
+++ gse/cart/views.py
@@ -1,13 +1,14 @@
+from django.http import Http404
 from drf_spectacular.utils import extend_schema
 from rest_framework import status
+from rest_framework.generics import GenericAPIView
 from rest_framework.generics import RetrieveAPIView, DestroyAPIView
 from rest_framework.response import Response
-from rest_framework.generics import GenericAPIView
 
 from gse.docs.serializers.doc_serializers import ResponseSerializer
 from gse.permissions.permissions import IsAdminOrOwner
 from gse.utils.format_errors import format_errors
-from .selectors import get_all_carts, get_all_cart_items
+from .selectors import get_all_carts, get_all_cart_items, get_cart_by_item_id, get_cart_item_by_id
 from .serializers import (
     CartSerializer,
     CartItemAddSerializer,
@@ -64,3 +65,17 @@ class CartItemDeleteAPI(DestroyAPIView):
     serializer_class = CartItemSerializer
     permission_classes = [IsAdminOrOwner]
     queryset = get_all_cart_items()
+
+    def get_object(self):
+        item_id = self.kwargs.get('pk')
+        cart = get_cart_by_item_id(item_id=item_id)
+        return cart
+
+    def delete(self, request, *args, **kwargs):
+        cart_item = get_cart_item_by_id(item_id=kwargs.get('pk'))
+        if cart_item is None:
+            raise Http404('آیتم با این مشخصات یافت نشد.')
+        cart_item.delete()
+        return Response(
+            status=status.HTTP_204_NO_CONTENT
+        )
