7fc9edb8eb12c74a4c56712959d4f9a5fac9db12
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-11-30 17:55:02 +0330

    fix

--- gse/orders/signals.py
+++ gse/orders/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
 from .models import Order
@@ -10,3 +10,10 @@ def check_order_item_count(sender, instance, created, **kwargs):
         item_count = instance.items.all().count()
         if item_count == 0:
             instance.delete()
+
+
+@receiver(pre_delete, sender=Order)
+def check_coupon_usage_limit(sender, instance, **kwargs):
+    if instance.coupon is not None:
+        instance.coupon.max_usage_limit += 1
+        instance.coupon.save()
