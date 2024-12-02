54dfe2450ddcf98c9287ab7471a4f66f11b6f773
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-02 22:52:06 +0330

    feat(selectors): add a selector that returns all payment objects.

--- gse/payment/selectors.py
+++ gse/payment/selectors.py
@@ -3,3 +3,7 @@ from .models import Payment
 
 def get_payment_by_id(payment_id: int) -> Payment | None:
     return Payment.objects.filter(id=payment_id).first()
+
+
+def get_all_payments() -> list[Payment]:
+    return Payment.objects.all()
