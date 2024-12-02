a1bfd97651621316f3ed6042f0f4477e9976e424
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-02 19:56:12 +0330

    feat(service): add payment request service.

--- gse/payment/services.py
+++ gse/payment/services.py
@@ -1,6 +1,9 @@
+import json
 import os
 from decimal import Decimal
 
+import requests
+from django.conf import settings
 from django.db import transaction
 
 from gse.orders.models import Order
@@ -35,3 +38,44 @@ def set_payment_status(payment: Payment, status: str) -> Payment:
     payment.status = status
     payment.save()
     return payment
+
+
+def payment_request(amount: int, phone: str, email: str):
+    req_data = {
+        "merchant_id": settings.MERCHANT,
+        "amount": amount,
+        "callback_url": settings.CALLBACK_URL,
+        "description": 'description',
+        "metadata": {"mobile": phone, "email": email}
+    }
+    req_header = {"accept": "application/json",
+                  "content-type": "application/json'"}
+    try:
+        req = requests.post(url=settings.ZP_API_REQUEST, data=json.dumps(req_data), headers=req_header)
+        authority = req.json()['data']['authority']
+        if len(req.json()['errors']) == 0:
+            return {
+                'authority': authority,
+                'url': settings.ZP_API_STARTPAY + authority,
+                'code': 200
+            }
+        else:
+            e_code = req.json()['errors']['code']
+            e_message = req.json()['errors']['message']
+            return {
+                'ErrorName': 'Unknown',
+                'Error': e_message,
+                'code': e_code
+            }
+    except requests.exceptions.ConnectionError:
+        return {
+            'ErrorName': 'ConnectionError',
+            'Error': 'اتصال ناموفق',
+            'code': 400
+        }
+    except requests.exceptions.Timeout:
+        return {
+            'ErrorName': 'TimeOut',
+            'Error': 'عملیات ناموفق',
+            'code': 400
+        }
