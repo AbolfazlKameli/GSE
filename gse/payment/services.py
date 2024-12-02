f66af5949ee61a53d44ae72fc160d6327d40bc3f
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-02 19:54:48 +0330

    feat(service): add create payment service.

---
+++ gse/payment/services.py
@@ -0,0 +1,30 @@
+import os
+from decimal import Decimal
+
+from django.db import transaction
+
+from gse.orders.models import Order
+from .models import Payment
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', f'core.settings.settings')
+
+
+@transaction.atomic()
+def create_payment(
+        *,
+        order: Order,
+        authority_id: str,
+        amount: Decimal,
+        ref_id: str = None,
+        response_json=None,
+        response_code: int = None
+) -> Payment:
+    payment: Payment = Payment.objects.create(
+        order=order,
+        authority_id=authority_id,
+        ref_id=ref_id,
+        amount=amount,
+        response_json=response_json,
+        response_code=response_code
+    )
+    return payment
