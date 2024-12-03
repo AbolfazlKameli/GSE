b4acbf1eb554707c8e8e824d4a253e7977c0060d
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-03 17:33:17 +0330

    feat(selectors): add a selector that returns admins and supporter users ids.

--- gse/users/selectors.py
+++ gse/users/selectors.py
@@ -1,3 +1,6 @@
+from django.core.cache import cache
+
+from .choices import USER_ROLE_ADMIN, USER_ROLE_SUPPORT
 from .models import User
 
 
@@ -6,3 +9,12 @@ def get_user_by_email(email: str) -> User | None:
         return User.objects.get(email__iexact=email)
     except User.DoesNotExist:
         return None
+
+
+def get_admins_and_supporters_ids() -> list[int]:
+    cached_data = cache.get("admins_and_supporters_ids")
+    if cached_data is not None:
+        return cached_data
+    result = [user.id for user in User.objects.filter(role__in=[USER_ROLE_ADMIN, USER_ROLE_SUPPORT])]
+    cache.set("admins_and_supporters_ids", result, timeout=3600)
+    return result
