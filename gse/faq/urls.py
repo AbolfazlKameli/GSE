ae25581271aaf5b0bdcbe350c4b0e79c6a079c96
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-07 19:27:27 +0330

    feat(API): add question create API.

--- gse/faq/urls.py
+++ gse/faq/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'faq'
 urlpatterns = [
     path('questions/', views.QuestionListAPI.as_view(), name='questions_list'),
+    path('<int:product_id>/questions/add/', views.QuestionCreateAPI.as_view(), name='question_create'),
 ]
--- gse/faq/views.py
+++ gse/faq/views.py
@@ -1,6 +1,11 @@
-from rest_framework.generics import ListAPIView
-from rest_framework.permissions import IsAdminUser
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 
+from gse.products.models import Product
+from gse.utils.format_errors import format_errors
 from .selectors import get_all_questions
 from .serializers import QuestionSerializer
 
@@ -12,3 +17,24 @@ class QuestionListAPI(ListAPIView):
     serializer_class = QuestionSerializer
     permission_classes = [IsAdminUser]
     queryset = get_all_questions()
+
+
+class QuestionCreateAPI(GenericAPIView):
+    """
+    API for creating questions, accessible only to authenticated users.
+    """
+    serializer_class = QuestionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        product: Product = get_object_or_404(Product, id=self.kwargs.get('product_id'))
+        if serializer.is_valid():
+            serializer.save(owner=request.user, product=product)
+            return Response(
+                data={'data': {'message': 'سوال با موفقیت ایجاد شد.'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': format_errors(serializer.errors)}}
+        )
