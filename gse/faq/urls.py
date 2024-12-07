b6935686a9cb344a4517f2240b6812744a945d6c
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-07 19:14:36 +0330

    feat(API): add question list API.

--- gse/faq/urls.py
+++ gse/faq/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'faq'
 urlpatterns = [
-
+    path('questions/', views.QuestionListAPI.as_view(), name='questions_list'),
 ]
--- gse/faq/views.py
+++ gse/faq/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAdminUser
 
-# Create your views here.
+from .selectors import get_all_questions
+from .serializers import QuestionSerializer
+
+
+class QuestionListAPI(ListAPIView):
+    """
+    API for listing questions, accessible only to admin users.
+    """
+    serializer_class = QuestionSerializer
+    permission_classes = [IsAdminUser]
+    queryset = get_all_questions()
