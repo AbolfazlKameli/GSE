ef04d2538a10dc1ff952e8e8ed6d04158a3de057
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-07 18:36:21 +0330

    feat(answer): add answer model.

--- gse/faq/admin.py
+++ gse/faq/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Answer
+
+
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    fields = ('body',)
 
 
 @admin.register(Question)
 class QuestionAdmin(admin.ModelAdmin):
     list_display = ('product__id', 'owner__id', 'created_date', 'status')
     list_filter = ('status',)
+    inlines = [AnswerInline]
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('question__id', 'created_date', 'body')
--- gse/faq/models.py
+++ gse/faq/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
-from gse.users.models import User
 from gse.products.models import Product
+from gse.users.models import User
 from .choices import QUESTION_STATUS_CHOICES, QUESTION_STATUS_PENDING
 
 
@@ -10,6 +10,15 @@ class Question(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='questions')
     body = models.CharField(max_length=255)
     status = models.CharField(choices=QUESTION_STATUS_CHOICES, default=QUESTION_STATUS_PENDING, max_length=10)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_date',)
+
+
+class Answer(models.Model):
+    question = models.OneToOneField(Question, on_delete=models.CASCADE, related_name='answers')
+    body = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
