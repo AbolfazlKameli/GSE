e0f02ff7e8a87c376178ab2486e111a726c63898
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-08 18:34:28 +0330

    feat(models): add ticket model and admin page.

--- gse/ticket/admin.py
+++ gse/ticket/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ticket
+
+
+@admin.register(Ticket)
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('owner__id', 'title', 'created_date', 'status')
+    list_filter = ('status',)
--- gse/ticket/models.py
+++ gse/ticket/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from gse.users.models import User
+from .choices import TICKET_STATUS_CHOICES, TICKET_STATUS_PENDING
+
+
+class Ticket(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    status = models.CharField(choices=TICKET_STATUS_CHOICES, default=TICKET_STATUS_PENDING, max_length=10)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created_date',)
