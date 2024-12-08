dc0470902e020c68e535198a8330ef8bdce7f321
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-08 19:23:43 +0330

    feat(API): add ticket create API.

--- gse/ticket/urls.py
+++ gse/ticket/urls.py
@@ -6,4 +6,5 @@ app_name = 'ticket'
 urlpatterns = [
     path('', views.TicketsListAPI.as_view(), name='tickets_list'),
     path('<int:pk>/', views.TicketRetrieveAPI.as_view(), name='ticket_retrieve'),
+    path('add/', views.TicketCreateAPI.as_view(), name='ticket_create')
 ]
--- gse/ticket/views.py
+++ gse/ticket/views.py
@@ -1,9 +1,11 @@
-from rest_framework.generics import ListAPIView, RetrieveAPIView
-from rest_framework.permissions import IsAdminUser
+from rest_framework import status
+from rest_framework.generics import ListAPIView, RetrieveAPIView, GenericAPIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.response import Response
 
 from .selectors import get_all_tickets
 from .serializers import TicketsListSerializer, TicketSerializer
+from ..utils.format_errors import format_errors
 
 
 class TicketsListAPI(ListAPIView):
@@ -28,4 +30,25 @@ class TicketRetrieveAPI(RetrieveAPIView):
         return Response(
             data={'data': response.data},
             status=response.status_code
+        )
+
+
+class TicketCreateAPI(GenericAPIView):
+    """
+    API for creating tickets, accessible only to authenticated users.
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = TicketSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save(owner=request.user)
+            return Response(
+                data={'data': {'message': 'تیکت با موفقیت ثبت شد.'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': format_errors(serializer.errors)}},
+            status=status.HTTP_400_BAD_REQUEST
         )
