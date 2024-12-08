0f18672fde65a53a1472cb070f46c4ac13a419df
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-08 21:30:55 +0330

    feat(services): add a service that removes an answer and changes the ticket status.

--- gse/ticket/services.py
+++ gse/ticket/services.py
@@ -1,11 +1,11 @@
 from django.db import transaction
 
+from .choices import TICKET_STATUS_ANSWERED, TICKET_STATUS_PENDING
 from .models import Ticket, TicketAnswer
-from .choices import TICKET_STATUS_ANSWERED
 
 
 @transaction.atomic
-def submit_answer(ticket: Ticket, title: str, body: str):
+def submit_answer(ticket: Ticket, title: str, body: str) -> TicketAnswer:
     answer = TicketAnswer.objects.create(
         title=title,
         body=body,
@@ -14,3 +14,11 @@ def submit_answer(ticket: Ticket, title: str, body: str):
     ticket.status = TICKET_STATUS_ANSWERED
     ticket.save()
     return answer
+
+
+@transaction.atomic
+def remove_answer(ticket: Ticket, answer: TicketAnswer) -> Ticket:
+    answer.delete()
+    ticket.status = TICKET_STATUS_PENDING
+    ticket.save()
+    return ticket
