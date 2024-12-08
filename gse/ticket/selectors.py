776c53e457a00ad87d401e2acc82f1df63b37a68
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-08 19:51:32 +0330

    feat(selectors): add a selector that returns an answer by its id.

--- gse/ticket/selectors.py
+++ gse/ticket/selectors.py
@@ -1,5 +1,9 @@
-from .models import Ticket
+from .models import Ticket, TicketAnswer
 
 
 def get_all_tickets() -> list[Ticket]:
     return Ticket.objects.select_related('answers').all()
+
+
+def get_answer_by_id(answer_id: int) -> TicketAnswer | None:
+    return TicketAnswer.objects.filter(id=answer_id).first()
