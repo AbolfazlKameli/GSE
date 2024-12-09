e47d00da1ce84d024246b4bfd30d517f4d9399cb
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-09 22:36:22 +0330

    fix(API): add phone number verification.

--- gse/users/tasks.py
+++ gse/users/tasks.py
@@ -10,4 +10,12 @@ def send_verification_email(email_address: str, content: str, subject: str):
     content = f'{content}\n{otp_code}'
     # if action == 'reset_password':
     #     code = f"http://{settings.DOMAIN}{reverse('users:set-password', args=[otp_code])}"
-    send_email.send_link(email=email_address, content=content, subject=subject)
+    senders.send_link(email=email_address, content=content, subject=subject)
+
+
+@shared_task
+def send_verification_sms(phone_number: str):
+    otp_code = generate_otp_code(email=phone_number)
+    content = f'کد تایید شما:\n {otp_code}'
+    result = senders.send_sms(phone_number=phone_number, content=content)
+    return result
--- gse/utils/senders.py
+++ gse/utils/senders.py
@@ -15,7 +15,12 @@ def send_link(*, email: str, content: str, subject: str):
 
 def send_sms(*, phone_number: str, content: str):
     api = KavenegarAPI(config('KAVENEGAR_API_KEY'))
-    sender = config('KAVENEGAR_PHONE_NUMBER', default='2000660110')
+    sender = config('KAVENEGAR_PHONE_NUMBER', default='2000500666')
     params = {'sender': sender, 'receptor': phone_number, 'message': content}
-    response = api.sms_send(params)
+    try:
+        response = api.sms_send(params)
+    except APIException:
+        return False
+    except HTTPException:
+        return False
     return response
