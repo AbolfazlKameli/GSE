916fa257ca0ded7db7d1de6e9a19c7a67885d8c9
Abolfazlkameli <abolfazlkameli0@gmail.com>
2024-12-09 18:48:28 +0330

    feat(API): add website attr create API.

--- gse/website/urls.py
+++ gse/website/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 app_name = 'website'
 urlpatterns = [
-
+    path('attributes/add/', views.WebsiteAttributeCreateAPI.as_view(), name='website_attr_create'),
 ]
--- gse/website/views.py
+++ gse/website/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import GenericAPIView
+from rest_framework.response import Response
 
-# Create your views here.
+from gse.permissions.permissions import IsAdminOrSupporter
+from gse.utils.format_errors import format_errors
+from gse.website.serializers import WebsiteSerializer
+
+
+class WebsiteAttributeCreateAPI(GenericAPIView):
+    """
+    API for creating website attribute.
+    """
+    serializer_class = WebsiteSerializer
+    permission_classes = [IsAdminOrSupporter]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data={'data': {'message': 'ویژگی وبسایت با موفقیت ایحاد و ثبت شد.'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': format_errors(serializer.errors)}},
+            status=status.HTTP_400_BAD_REQUEST
+        )
