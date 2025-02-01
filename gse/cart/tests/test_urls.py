b5db2ad345970dbadd4e9466ed512af9983690b4
AbolfazlKameli <abolfazlkameli0@gmail.com>
2025-02-01 18:52:24 +0330

    refactor(test): refactor url test in cart app.

--- gse/cart/tests/test_urls.py
+++ gse/cart/tests/test_urls.py
@@ -1,60 +1,36 @@
 from django.urls import reverse, resolve
-from rest_framework import status
 from rest_framework.test import APITestCase
 
-from gse.cart.views import (
-    CartRetrieveAPI,
-    CartItemAddAPI,
-    CartItemDeleteAPI
-)
+from gse.cart.views import *
 
 
-class CartRetrieveAPITest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.url = reverse('cart:cart_retrieve')
-
-    def test_url_resolves_to_retrieveapi(self):
-        resolver = resolve(self.url)
+class EndpointsTest(APITestCase):
+    def test_cart_retrieve_resolves_to_cart_retrieve_api(self):
+        url = reverse('cart:cart_retrieve')
+        resolver = resolve(url)
+        response = self.client.get(url)
         self.assertEqual(resolver.func.view_class, CartRetrieveAPI)
-
-    def test_retrieveapi_response(self):
-        response = self.client.get(self.url)
         self.assertIn(
             response.status_code,
-            [status.HTTP_404_NOT_FOUND, status.HTTP_200_OK, status.HTTP_401_UNAUTHORIZED]
+            [status.HTTP_200_OK, status.HTTP_401_UNAUTHORIZED, status.HTTP_404_NOT_FOUND]
         )
 
-
-class CartItemAddAPITest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.url = reverse('cart:cart_create')
-
-    def test_url_resolves_to_addapi(self):
-        resolver = resolve(self.url)
+    def test_cart_create_resolves_to_cart_item_add_api(self):
+        url = reverse('cart:cart_create')
+        resolver = resolve(url)
+        response = self.client.post(url)
         self.assertEqual(resolver.func.view_class, CartItemAddAPI)
-
-    def test_addapi_response(self):
-        response = self.client.post(self.url)
         self.assertIn(
             response.status_code,
-            [status.HTTP_201_CREATED, status.HTTP_400_BAD_REQUEST, status.HTTP_401_UNAUTHORIZED]
+            [status.HTTP_201_CREATED, status.HTTP_401_UNAUTHORIZED, status.HTTP_404_NOT_FOUND]
         )
 
-
-class CartItemDeleteAPITest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.url = reverse('cart:cart_delete', args=[20])
-
-    def test_url_resolves_to_deleteapi(self):
-        resolver = resolve(self.url)
+    def test_cart_delete_resolves_to_cart_item_delete_api(self):
+        url = reverse('cart:cart_delete', args=[20])
+        resolver = resolve(url)
+        response = self.client.delete(url)
         self.assertEqual(resolver.func.view_class, CartItemDeleteAPI)
-
-    def test_deleteapi_response(self):
-        response = self.client.delete(self.url)
         self.assertIn(
             response.status_code,
-            [status.HTTP_401_UNAUTHORIZED, status.HTTP_204_NO_CONTENT, status.HTTP_404_NOT_FOUND]
+            [status.HTTP_204_NO_CONTENT, status.HTTP_401_UNAUTHORIZED, status.HTTP_404_NOT_FOUND]
         )
