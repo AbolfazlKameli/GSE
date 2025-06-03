2ceeed5fc9fa552236587473aafe343b4f20c2e6
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-06-03 20:14:46 +0330

    fix: Remove logic from serializer and tagged the document.

--- gse/ticket/serializers.py
+++ gse/ticket/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 
 from .models import Ticket, TicketAnswer
-from .services import submit_answer
 
 
 class TicketAnswerSerializer(serializers.ModelSerializer):
@@ -9,13 +8,6 @@ class TicketAnswerSerializer(serializers.ModelSerializer):
         model = TicketAnswer
         fields = '__all__'
         read_only_fields = ('ticket',)
-
-    def save(self, **kwargs):
-        submit_answer(
-            ticket=kwargs.get('ticket'),
-            title=self.validated_data.get('title'),
-            body=self.validated_data.get('body')
-        )
 
 
 class TicketSerializer(serializers.ModelSerializer):
