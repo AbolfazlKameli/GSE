2990f87c68dc7a515c0d608b0eefe7957a34e358
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-11-07 17:57:42 +0330

    fix: Fix settings.
    - refactor zarinpal sandbox and api definition.
    
    - using strict False in production settings.
    
    - fix jwt token expire time.

--- core/configs/zarin_pal.py
+++ core/configs/zarin_pal.py
@@ -3,12 +3,9 @@ from decouple import config
 DEBUG = config('DJANGO_DEBUG', default=False, cast=bool)
 
 MERCHANT = config('ZP_MERCHANT', default='00000000-0000-0000-0000-000000000000')
-if DEBUG:
-    sandbox = 'sandbox'
-    api = 'sandbox'
-else:
-    sandbox = 'www'
-    api = 'api'
+
+sandbox = 'sandbox' if DEBUG else 'www'
+api = 'sandbox' if DEBUG else 'api'
 
 ZP_API_REQUEST = f"https://{api}.zarinpal.com/pg/v4/payment/request.json"
 ZP_API_VERIFY = f"https://{api}.zarinpal.com/pg/v4/payment/verify.json"
