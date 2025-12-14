e93f2a6f046d81690cc18f7753c293d0501ee9ac
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-12-14 18:19:10 +0330

    fix(cart): Fix add cart item service quantity addition.

--- gse/cart/services.py
+++ gse/cart/services.py
@@ -1,15 +1,14 @@
+from gse.products.models import Product
+
 from .models import Cart, CartItem
 from .selectors import get_cart_item_by_product_id
 
 
-def add_cart_item(cart: Cart, item_data: dict) -> CartItem:
-    product = item_data.get('product')
-    quantity = item_data.get('quantity')
-
+def add_cart_item(cart: Cart, product: Product) -> CartItem:
     item_obj = get_cart_item_by_product_id(product_id=product.id, owner_id=cart.owner.id)
     if item_obj and (product.id == item_obj.product.id):
-        item_obj.quantity += quantity
+        item_obj.quantity += 1
         item_obj.save()
         return item_obj
 
-    return CartItem.objects.create(cart=cart, **item_data)
+    return CartItem.objects.create(cart=cart, product=product)
